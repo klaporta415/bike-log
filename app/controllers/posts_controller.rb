49f5b4a8b24435419826bd12b5343bc17ec1d80d
Kaley La Porta <kaley.laporta@gmail.com>
2017-05-28 18:17:41 -0500

    Recreated Post model - was plural

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,27 @@
 class PostsController < ApplicationController
+  # all posts on index page
   def index
   end
 
+# create new post
   def new
-
   end
+
+# saving post to database
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # Using strong params - define which parameters you will accept
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
 end
