82cde1fbdc7bee0866b7ccc413c7208e356e6d49
Kaley La Porta <kaley.laporta@gmail.com>
2017-05-28 20:32:07 -0500

    Add stylesheets that went missing... and about page with route

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  # if post gets deleted, comments get deleted as well
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 10 }
 end
