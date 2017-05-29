61215d1c6321c3e1671a26a908ef7484cda1e1e9
Kaley La Porta <kaley.laporta@gmail.com>
2017-05-28 20:05:02 -0500

    model and migration for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 BikeLog::Application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+
   root 'posts#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
